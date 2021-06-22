a9f9710815ef893ff617c93cbbf0ac5cdcb5cb94
呓喵酱 <i@iotcat.me>
2021-06-22 19:15:12 +0800

    docker-compose aarch64

--- setup.sh
+++ setup.sh
@@ -78,9 +78,8 @@ systemctl enable docker
 systemctl start docker
 #
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L https://raw.githubusercontent.com/PaiCoop/setup-env/main/lib/docker-compose_$(uname -m) -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 #############################
 #  Firewall Setup           
 #############################
